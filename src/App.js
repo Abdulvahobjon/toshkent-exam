49c2b8406da65f53981a5256e29e566efed71997
Abdulvahobjon <abdulvahob0714@gmail.com>
2023-11-04 11:43:26 +0500

    commid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import Parametrs from "./parametrs";
+import data1 from "./data1";
+import { useFetch } from "./hooks/useFetch";
+
 import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
   Route,
 } from "react-router-dom";
-
+import { useState } from "react";
 import Home from "./Home";
 import Layout from "./layout/Layout";
 
 export default function App() {
+  const { data, isPanding, error } = useFetch("http://localhost:3000/data");
+  let [info, setInfo] = useState(data);
+  let [info1, setInfo1] = useState(data1);
   const routes = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<Layout/>}>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home info1={info1} setInfo1={setInfo1} info={info} setInfo={setInfo} />} />
         <Route path="/:id" element={<Parametrs />} />
       </Route>
     )
